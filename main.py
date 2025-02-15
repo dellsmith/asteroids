5e21bd0e09c30cb17d4384c1b6db9ddbe7bde93b
riddell999 <dell.smith999@gmail.com>
2025-02-15 13:30:37 -0600

    constants.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+print("Starting asteroids!")
+print(f"Screen width = {SCREEN_WIDTH}")
+print(f"Screen height = {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
+
+from constants import *
