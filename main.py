92323bc13bb09e773f06c450233ce18d35cc012c
riddell999 <dell.smith999@gmail.com>
2025-02-15 14:52:33 -0600

    rotating triangle

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-print("Starting asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        dt = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        player.update(dt)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill(color=0, rect=None, special_flags=0)
-    pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        
 
 if __name__ == "__main__":
     main()
